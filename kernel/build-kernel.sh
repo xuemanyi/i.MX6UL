29e1388da8088fd7674dc9eaf61534cd918af255
sucre <guoshengw24@gmail.com>
2026-01-18 17:09:45 +0800

    build(kernel): add build-kernel.sh for i.MX6UL kernel build

--- kernel/build-kernel.sh
+++ kernel/build-kernel.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
+
+# Clean the kernel source tree
+# This removes all generated files and restores the tree to a pristine state
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
+
+# Load the default configuration for i.MX v7 based ARM platforms
+# This provides a baseline kernel configuration for i.MX6UL
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- imx_v7_defconfig
+
+# Open the interactive kernel configuration menu
+# Used to enable/disable kernel features manually
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
+
+# Build the entire kernel
+# -j16 enables parallel build with 16 jobs to speed up compilation
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- all -j16
