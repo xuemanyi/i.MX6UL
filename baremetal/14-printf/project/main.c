81745a385d668285b07a40ec9d3a7d5cf67d43d5
sucre <guoshengw24@gmail.com>
2026-05-24 17:26:11 +0800

    Fix: Fix compile error

--- baremetal/14-printf/project/main.c
+++ baremetal/14-printf/project/main.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
-
 #include "bsp_clk.h"
 #include "bsp_delay.h"
 #include "bsp_led.h"
 #include "bsp_beep.h"
 #include "bsp_int.h"
 #include "bsp_uart.h"
+#include "stdio.h"
 
 /*
  * Bare-metal UART printf/scanf demo.
