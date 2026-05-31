8d3b960b7329f5c08ece6324f13a97ddb88efe4c
sucre <guoshengw24@gmail.com>
2026-05-31 21:44:49 +0800

    feat(spi): add SPI communication example

---
+++ baremetal/18-spi/imx6ul/cc.h
@@ -0,0 +1,35 @@
+#ifndef __CC_H
+#define __CC_H
+
+/*
+ * Basic type definitions for the NXP SDK port.
+ */
+
+#define __I     volatile const
+#define __O     volatile
+#define __IO    volatile
+
+#define ON      1
+#define OFF     0
+
+typedef signed char             int8_t;
+typedef signed short int        int16_t;
+typedef signed int              int32_t;
+typedef signed long long int    int64_t;
+
+typedef unsigned char           uint8_t;
+typedef unsigned short int      uint16_t;
+typedef unsigned int            uint32_t;
+typedef unsigned long long int  uint64_t;
+
+typedef signed char             s8;
+typedef signed short int        s16;
+typedef signed int              s32;
+typedef signed long long int    s64;
+
+typedef unsigned char           u8;
+typedef unsigned short int      u16;
+typedef unsigned int            u32;
+typedef unsigned long long int  u64;
+
+#endif /* __CC_H */
