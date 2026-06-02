d82036757ae733580a7afb8b384ccdc13e0ded21
sucre <guoshengw24@gmail.com>
2026-06-02 23:36:33 +0800

    feat: Add compile scripts

---
+++ bootloader/imx6ull_nand.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+TOOLCHAIN_DIR=/usr/local/arm/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf
+
+export PATH=${TOOLCHAIN_DIR}/bin:$PATH
+export ARCH=arm
+export CROSS_COMPILE=arm-linux-gnueabihf-
+
+which ${CROSS_COMPILE}gcc
+${CROSS_COMPILE}gcc -v
+
+make distclean
+
+make mx6ull_14x14_ddr256_nand_defconfig
+# make mx6ull_14x14_ddr512_emmc_defconfig
+
+make V=1 -j12
