6fc87413a8cf71466290a0be8d61b16692b5e3b2
sucre <guoshengw24@gmail.com>
2026-07-09 07:29:45 +0800

    style: remove LCD file header comments

--- baremetal/15-lcd/bsp/lcd/bsp_lcd.c
+++ baremetal/15-lcd/bsp/lcd/bsp_lcd.c
@@ -1,8 +1,3 @@
-/***************************************************************
-Copyright © zuozhongkai Co., Ltd. 1998-2019. All rights reserved.
-文件名  : bsp_lcd.c
-描述    : LCD驱动文件（优化版，功能保持不变）。
-***************************************************************/
 #include "bsp_lcd.h"
 #include "bsp_gpio.h"
 #include "bsp_delay.h"
